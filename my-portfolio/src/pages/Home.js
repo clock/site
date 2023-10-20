f9df1dbbc667109c68e6473af62fd4356a6a0720
aiden <aidenmods123@gmail.com>
2023-10-20 16:09:00 -0400

    Update Home.js

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -26,7 +26,7 @@ export default function Home() {
       title: 'Software Loader',
       description: 'Software loader excluding access from non-paying users or users without an account. Disallow the ability to crack / pirate software. This is a full stack application with an API, Nextjs React admin panel using Tailwind CSS, SQL database with locking, and finally, a C++ client is all used.',
       image: project2,
-      languages: ['C++', 'Node.js', 'SQL', 'React', 'Tailwind'],
+      languages: ['C++', 'Node.js', 'SQL', 'React'],
       link: '../site/#writeups#loader',
     },
   ];
